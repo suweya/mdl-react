9d2593264eb72c7feec966291eaaf8e81a0b6ccd
suweya <suweya89@gmail.com>
2016-07-01 10:50:06 +0800

    add src folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
-var webpack = require('webpack')
+var webpack = require('webpack');
+var path = require('path');
 
 module.exports= {
 
-	entry: './main.js',
+	entry: [
+		'./src/main.js',
+		'webpack/hot/dev-server',
+    	'webpack-dev-server/client?http://localhost:8080'
+    ],
 
 	output: {
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+		path: path.join(__dirname, 'static'),
+    	publicPath: '/static/'
 	},
 
 	module: {
 	    loaders:[
 	      {
 	        test: /\.js[x]?$/,
-	        exclude: /node_modules/,
+	        include: path.join(__dirname, 'src'),
 	        loader: 'babel-loader?presets[]=es2015&presets[]=react'
 	      },
 	      { test: /\.css$/, loader: 'style-loader!css-loader?modules' },
@@ -20,6 +27,7 @@ module.exports= {
   	},
 
   	plugins: [
-    	new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.bundle.js')
+    	new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.bundle.js'),
+    	new webpack.HotModuleReplacementPlugin()
   	]
 }
